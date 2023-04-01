fb8d5e8ffcbb897753049b78a96af55eac722286
Lincoln Ferreira <lincoln.carmo@alpe.com.br>
2023-04-01 16:12:32 -0300

    atualização de containers, buttons e text styles

--- src/components/atoms/container/index.tsx
+++ src/components/atoms/container/index.tsx
@@ -18,7 +18,7 @@ export const Container = styled.section<{
 `;
 
 export const ContainerFluid = styled.section<{
-	height: string | number;
+	height?: string | number;
 	display?: "flex" | "block" | "inline";
 	justify?: "center" | "space-between" | "left" | "space-around";
 }>`
@@ -29,29 +29,22 @@ export const ContainerFluid = styled.section<{
 	align-items: center;
 	margin: 0 auto;
 	overflow: hidden;
-	background-color: #cb7ae8;
 	@media (min-width: ${p => p.theme.breakpoint.xsm}) {
-		background-color: #e8be7a;
 		width: 90%;
 	}
 	@media (min-width: ${p => p.theme.breakpoint.sm}) {
-		background-color: #e87a7a;
 		width: ${p => p.theme.container.sm};
 	}
 	@media (min-width: ${p => p.theme.breakpoint.md}) {
-		background-color: #c0e87a;
 		width: ${p => p.theme.container.md};
 	}
 	@media (min-width: ${p => p.theme.breakpoint.lg}) {
-		background-color: #7ae88c;
 		width: ${p => p.theme.container.lg};
 	}
 	@media (min-width: ${p => p.theme.breakpoint.xlg}) {
-		background-color: #7ae8e3;
 		width: ${p => p.theme.container.xlg};
 	}
 	@media (min-width: ${p => p.theme.breakpoint.xxlg}) {
-		background-color: #7e7ae8;
 		width: ${p => p.theme.container.xxlg};
 	}
 `;
