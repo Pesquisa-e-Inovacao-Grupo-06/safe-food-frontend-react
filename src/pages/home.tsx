57c31dc56edb9cdada91071af2c9a2be6fecfd2e
Lincoln Ferreira <lincoln.carmo@alpe.com.br>
2023-04-01 17:08:42 -0300

    acrescentando banner mobile

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,16 @@
 import Header from "../components/molecules/header/index";
 import { FooterOrganism } from "../components/organisms/footer/footer-organism";
+import { BannerMobilePlatform } from "../components/organisms/banner-mobile-platform/banner-mobile-platform";
 import { ContainerFluid } from "../components/atoms/container/index";
 function Home() {
 	return (
 		<>
 			<Header />
-			<div
-				style={{ paddingTop: "75px", marginLeft: "248px", marginRight: "248px" }}
-			>
-				<FooterOrganism />
+			<div style={{ paddingTop: "75px" }}>
+				<ContainerFluid>
+					<BannerMobilePlatform />
+					<FooterOrganism />
+				</ContainerFluid>
 			</div>
 		</>
 	);
