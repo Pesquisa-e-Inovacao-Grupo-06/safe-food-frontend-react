9bb15382d7353c4558ba1f1f664c88f9dc60a933
Guivicsan <guilherme.santos@sptech.school>
2023-04-09 00:01:48 -0300

    Implementação da sidebar-establishment

---
+++ src/components/molecules/sidebar-establishment/layout/styles.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const SLayout = styled.div`
+	display: grid;
+	grid-template-columns: 0fr 1fr 0fr;
+
+	@media screen and (max-width: 600px) {
+		grid-template-columns: none;
+	}
+`;
+
+export const SMain = styled.main`
+	padding: calc(8px * 2);
+	overflow-y: scroll;
+	height: 100dvh;
+
+	/* Scrollbar modification */
+
+	::-webkit-scrollbar {
+		width: 8px;
+	}
+
+	/* Track */
+
+	::-webkit-scrollbar-track {
+		background-color: ${p =>
+			p.theme.name == "light"
+				? p.theme.colors.light_gray[200]
+				: p.theme.colors.dark_gray[600]};
+	}
+
+	/* Handle */
+
+	::-webkit-scrollbar-thumb {
+		background-color: ${p =>
+			p.theme.name == "light"
+				? p.theme.colors.light_gray[600]
+				: p.theme.colors.dark_gray[1000]};
+		border-radius: 50px;
+		border: 3px solid
+			${p =>
+				p.theme.name == "light"
+					? p.theme.colors.light_gray[200]
+					: p.theme.colors.dark_gray[600]};
+	}
+
+	/* Handle on Hover */
+
+	::-webkit-scrollbar-thumb:hover {
+		background-color: ${p =>
+			p.theme.name == "light"
+				? p.theme.colors.light_gray[800]
+				: p.theme.colors.black};
+	}
+
+	h1 {
+		font-size: 14px;
+	}
+
+	@media screen and (max-width: 600px) {
+		overflow-y: initial;
+	}
+`;
