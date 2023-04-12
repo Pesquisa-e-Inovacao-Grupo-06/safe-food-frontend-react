15b895a4fab24284d191caf5cffdf420b88e086a
Lincoln Ferreira <lincoln.carmo@alpe.com.br>
2023-04-12 20:35:37 -0300

    merge

--- src/components/templates/home-template.tsx
+++ src/components/templates/home-template.tsx
@@ -5,7 +5,16 @@ import { MoreFavoritesTemplate } from "./more-favorites-template";
 import Header from "../molecules/header";
 import { Box } from "../atoms/box";
 import { CardEstablishmentFoodOTemplate } from "./card-establishment-food-organism";
-import { ContainerFluid } from "../atoms/container";
+import { Container, ContainerFluid } from "../atoms/container";
+import { Input } from "../atoms/input";
+import { Column } from "../atoms/column";
+import { TextField } from "../molecules/textfield";
+import { Button } from "../atoms/button";
+import { Title } from "@/styles/components/text/Title";
+import {
+	StyledColumn,
+	StyledRow,
+} from "../organisms/card-establishment-food/card-establishment-food-organism";
 
 export const HomeTemplate = ({}) => {
 	return (
@@ -14,6 +23,45 @@ export const HomeTemplate = ({}) => {
 			<ContainerFluid>
 				<Divider marginBottom="100px"></Divider>
 				<Box>
+					<Title>O Alimento restritivo que você pode confiar</Title>
+					<Box>
+						<StyledRow>
+							<StyledColumn>
+								<Container
+									size="sm"
+									height={"auto"}
+								>
+									<Column style={{ width: "70%" }}>
+										<TextField
+											type="email"
+											label={"O que vai querer hoje?"}
+											id={""}
+											required={false}
+											onChange={() => {}}
+											value={""}
+										></TextField>
+										<Divider
+											marginAll="10px"
+											color="transparent"
+										></Divider>
+										<TextField
+											type="email"
+											label={"localização:"}
+											id={""}
+											required={false}
+											onChange={() => {}}
+											value={""}
+										></TextField>
+										<Divider
+											marginAll="10px"
+											color="transparent"
+										></Divider>{" "}
+										<Button style={{ width: "100%" }}>Pesquisar</Button>
+									</Column>
+								</Container>
+							</StyledColumn>
+						</StyledRow>
+					</Box>
 					<CardEstablishmentFoodOTemplate />
 					{/* more-favorites */}
 					<MoreFavoritesTemplate />
