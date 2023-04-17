df55d3aceb3e63e39ce80208d2f67cb66249429c
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-04-16 22:46:18 -0300

    refactor: ajustando modal

--- src/pages/signIn.tsx
+++ src/pages/signIn.tsx
@@ -11,17 +11,15 @@ import { SizeLogo } from "@/components/atoms/logo";
 import { LogoAtom } from "@/components/atoms/logo";
 import { InputEmailSignUp } from "@/components/organisms/signup-consumer/inputs/InputEmailSignUpConsumer";
 import { InputValidator } from "@/app/util/validations/input-validator";
-
-
+import { UnderlineLink } from "@/components/atoms/underline-link";
+import { useSafeFoodTheme } from "@/app/contexts/SafeFoodThemeProvider";
 
 function SignIn() {
 	const [isModalVisible, setModalVisible] = useState(true);
-
+	const { colors } = useSafeFoodTheme().getTheme();
 	return (
 		<>
-			<Button onClick={() =>
-				setModalVisible(!isModalVisible)}>Abrir modal
-			</Button>
+			<Button onClick={() => setModalVisible(!isModalVisible)}>Abrir modal</Button>
 
 			<Modal
 				size="sm"
@@ -33,61 +31,103 @@ function SignIn() {
 			>
 				<Box
 					display="flex"
-					justify="center"
+					justify="left"
+					alignItems="baseline"
 					flexDiretion="column"
 					padding="20px"
 					height="90%"
 					margin="auto"
 					gap={"12px"}
-					maxWidth={"600px"}
+					maxWidth={"400px"}
 					alignSelf="center"
 				>
-
-					<img src="/src/assets/svg-logo.svg" alt="Logo-Safe-Food" />
-					
-					
-					<Title>Entrar</Title>
-
-					<span>Bem vindo de volta! Digite seu e-mail e senha abaixo para entrar.</span>
-
-					
-					<TextField
-						label="Email:"
-						required
-						id="email"
-						value=""
-						placeholder="email@exemple.com"
-						type="email"
-						name="general-email"
-						inputMode="email"
-						max={100}
-						min={10}
-						onChange={ev =>{}}
+					<img
+						style={{
+							marginLeft: -12,
+						}}
+						src="/src/assets/svg-logo.svg"
+						alt="Logo-Safe-Food"
 					/>
 
-					<TextField
-						label="Senha:"
-						required
-						id="password"
-						value=""
-						placeholder="*******"
-						type="password"
-						name="general-password"
-						inputMode="text"
-						max={100}
-						min={10}
-						onChange={ev =>{}}
-					/>
-	
-					<label htmlFor="">Esqueceu a senha?</label>	
+					<Subtitle large>Entrar</Subtitle>
 
-					<Button>Entrar</Button>
+					<TextAtom
+						typeText="text-md"
+						style={{
+							color: colors.dark_gray[200],
+						}}
+					>
+						Bem vindo de volta! Digite seu e-mail e senha abaixo para entrar.
+					</TextAtom>
 
-					<span>Não possui uma conta?</span>
-					<label htmlFor="">Cadastre-se</label>
+					<Box
+						margin="20px 0"
+						display="flex"
+						flexDiretion="column"
+						gap="20px"
+						style={{
+							alignItems: "flex-end",
+						}}
+					>
+						<TextField
+							label="Email:"
+							required
+							id="email"
+							value=""
+							placeholder="email@exemple.com"
+							type="email"
+							name="general-email"
+							inputMode="email"
+							max={100}
+							min={10}
+							onChange={ev => {}}
+						/>
 
-					<span>© 2023 Safe Food direitos reservados</span>
+						<TextField
+							label="Senha:"
+							required
+							id="password"
+							value=""
+							placeholder="*******"
+							type="password"
+							name="general-password"
+							inputMode="text"
+							max={100}
+							min={10}
+							onChange={ev => {}}
+						/>
 
+						<UnderlineLink href="forget-password">Esqueceu a senha?</UnderlineLink>
+
+						<Button
+							style={{
+								width: "100%",
+							}}
+						>
+							Entrar
+						</Button>
+					</Box>
+
+					<TextAtom>
+						Não possui uma conta?
+						<UnderlineLink
+							href="/signup"
+							style={{
+								marginLeft: 8,
+							}}
+						>
+							Cadastre-se
+						</UnderlineLink>
+					</TextAtom>
+
+					<TextAtom
+						typeText="text-sm"
+						style={{
+							color: colors.dark_gray[200],
+						}}
+					>
+						© 2023 Safe Food direitos reservados
+					</TextAtom>
 				</Box>
 			</Modal>
 		</>
