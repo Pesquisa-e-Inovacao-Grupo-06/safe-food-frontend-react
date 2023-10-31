e3de612c19c136a9be4bd6444f8a46559c4d0d3f
gdnarciso <gdnarciso@rd.com.br>
2023-10-31 19:52:50 -0300

    feat: test keys

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,16 +12,20 @@ export default defineConfig({
 		},
 	},
 	server: {
-		host: true,
-		strictPort: true,
-		port: 5173, // This is the port which we will use in docker
-		// Thanks @sergiomoura for the window fix
-		// add the next lines if you're using windows and hot reload doesn't work
-		watch: {
-			usePolling: true,
-		},
-		hmr: {
-			clientPort: 8080,
-		},
+		https: {
+			cert: fs.readFileSync("./.cert/cert.pem"),
+			key: fs.readFileSync("./.cert/key.pem")
+		}
+	// 	host: true,
+	// 	strictPort: true,
+	// 	port: 5173, // This is the port which we will use in docker
+	// 	// Thanks @sergiomoura for the window fix
+	// 	// add the next lines if you're using windows and hot reload doesn't work
+	// 	// watch: {
+	// 	// 	usePolling: true,
+	// 	// },
+	// 	hmr: {
+	// 		clientPort: 80,
+	// 	},
 	},
 });
